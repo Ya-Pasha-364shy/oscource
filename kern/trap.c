7d3fd64debb6e7263dda2aad37789f349a27df49
Ya-Pasha-364shy <pv-chernov2000@yandex.ru>
2024-03-18 22:49:40 +0300

    Lab 4 done.

--- kern/trap.c
+++ kern/trap.c
@@ -95,6 +95,8 @@ trapname(int trapno) {
 void
 trap_init(void) {
     // LAB 4: Your code here
+    extern void (*clock_thdlr)(void);
+    idt[IRQ_OFFSET + IRQ_CLOCK] = GATE(0, GD_KT, (uint64_t)&clock_thdlr, 0);
 
     /* Per-CPU setup */
     trap_init_percpu();
@@ -212,6 +214,11 @@ trap_dispatch(struct Trapframe *tf) {
         return;
     case IRQ_OFFSET + IRQ_CLOCK:
         // LAB 4: Your code here
+        rtc_check_status();
+        // прерывание было обработано
+        pic_send_eoi(IRQ_CLOCK);
+        // запуск планировщика
+        sched_yield();
         return;
     default:
         print_trapframe(tf);
